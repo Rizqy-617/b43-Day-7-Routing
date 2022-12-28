668584062cc890213217fdd952c3c2e18c4a4738
Rizqy-617 <rizqyandriansyah2205@gmail.com>
2022-12-28 11:43:12 +0700

    Finish Day 7

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/gorilla/mux"
+)
+
+var Data = map[string]interface{} {
+	"Title": "Personal Web",
+}
+
+func main() {
+	route := mux.NewRouter()
+
+	route.PathPrefix("/public/").Handler(http.StripPrefix("/public", http.FileServer(http.Dir("./public/"))))
+
+
+	route.HandleFunc("/", homePage).Methods("GET")
+	route.HandleFunc("/project", projectPage).Methods("GET")
+	route.HandleFunc("/project", addProject).Methods("POST")
+	route.HandleFunc("/contact", contactPage).Methods("GET")
+
+	fmt.Println("Server running on port:8080")
+	http.ListenAndServe("localhost:8080", route)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	tmpl, err := template.ParseFiles("view/index.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Message : " + err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, Data)
+}
+
+func projectPage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	tmpl, err := template.ParseFiles("view/myProject.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Message : " + err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, Data)
+}
+
+func addProject(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Project Name : " + r.PostForm.Get("project-name"))
+	fmt.Println("Start-date : " + r.PostForm.Get("start-date"))
+	fmt.Println("End-date : " + r.PostForm.Get("end-date"))
+	fmt.Println("Description : " + r.PostForm.Get("description"))
+	
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	tmpl, err := template.ParseFiles("view/contact.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Message : " + err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, Data)
+}
